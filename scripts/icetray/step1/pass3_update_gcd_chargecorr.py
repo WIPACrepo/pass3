71229b2df17d888ce311160e83892f1e46998f5c
R Snihur <gh999ic@gmail.com>
2025-04-14 12:48:52 -0500

    Add GCD audit and parse audit for nans that will be set to 1.

--- scripts/icetray/step1/pass3_update_gcd_chargecorr.py
+++ scripts/icetray/step1/pass3_update_gcd_chargecorr.py
@@ -1,28 +1,46 @@
 """Update GCD for pass3 processing, including
 
-* set SPE correction factors to 1.0 for ATWD and FADC charge corrections
+* Run audit for pass3 on input GCD file, written to file <ingcd_audit>
+* Parse warning messages in audit to get DOMs with nan value for ATWD or FADC charge corrections.
+  These DOMs had no other audit error, so the correction will be set to 1 in output GCD file. 
+* Set SPE correction factors to 1.0 for ATWD and FADC charge corrections
 -- set domcal.mean_atwd_charge = 1.0
 -- set domcal.mean_fadc_charge = 1.0
+* Run audit for pass3 on output GCD file, written to file <outgcd_audit>
 
-Usage:  python pass3_update_gcd_chargecorr.py <ingcd> <outgcd>
+Usage:  python pass3_update_gcd_chargecorr.py <ingcd> <outgcd> <ingcd_audit> <outgcd_audit>
 """
 
 import math
 import sys
 from icecube import icetray, dataclasses, dataio #NOQA: F401
+from icecube.offline_filterscripts.gcd_generation import get_nan_doms, run_gcd_audit_pass3
 
 icetray.logging.console()
 
-if len(sys.argv) !=3:
+if len(sys.argv) !=5:
     icetray.logging.log_error("pass3_update_gcd_chargecorr.py <ingcd> <outgcd>")
     sys.exit(0)
 
 infile = sys.argv[1]
 outfile = sys.argv[2]
+infile_audit = sys.argv[3]
+outfile_audit = sys.argv[4]
 
 icetray.logging.log_warn(f"Fixing GCD input file for Pass3: {infile}")
 icetray.logging.log_warn(f"Writing GCD: {outfile} ")
 
+icetray.logging.set_level("INFO")
+
+# GCD audit
+icetray.logging.rotating_files(infile_audit)
+run_gcd_audit_pass3(infile)
+
+# Get the list of doms that have a warning message from GCD audit for invalid mean charge value.
+# Such a dom therefore has no other audit error so the nan came from a failed fit.
+atwd, fadc = get_nan_doms(infile_audit)
+icetray.logging.log_info(f"atwd {atwd}")
+icetray.logging.log_info(f"fadc {fadc}")
 
 gcdfile_in = dataio.I3File(infile)
 
@@ -38,10 +56,14 @@ while gcdfile_in.more():
         cal_o = calitem.dom_cal  # type: ignore[attr-defined]
         for key, item in cal_o.items():
             old_atdw_cal.append(item.mean_atwd_charge)
-            if not math.isnan(item.mean_atwd_charge):
+            #if not math.isnan(item.mean_atwd_charge):
+            # Keep nan value for dom with audit warning
+            if key in atwd or not math.isnan(item.mean_atwd_charge):
                 item.mean_atwd_charge = 1.0
             old_fadc_cal.append(item.mean_fadc_charge)
-            if not math.isnan(item.mean_fadc_charge):
+            #if not math.isnan(item.mean_fadc_charge):
+            # Keep nan value for dom with audit warning
+            if key in fadc or not math.isnan(item.mean_fadc_charge):
                 item.mean_fadc_charge = 1.0
             cal_o[key] = item
         calitem.dom_cal = cal_o  # type: ignore[attr-defined]
@@ -51,3 +73,10 @@ while gcdfile_in.more():
 
 gcdfile_in.close()
 gcdfile_out.close()
+
+# GCD audit
+icetray.logging.rotating_files(outfile_audit)
+run_gcd_audit_pass3(outfile)
+#out_atwd, out_fadc = get_nan_doms(outfile_audit)
+#icetray.logging.log_info("out_atwd %", out_atwd)
+#icetray.logging.log_info("out_fadc %", out_fadc)
