29b58d21e5d91628af1454eb1e2ed769aab10dd0
Katherine Rawlins <krawlins@alaska.edu>
2025-08-26 11:40:03 -0500

    Add a module for swapping IceTop DOMCal's from one GCD into another GCD

---
+++ scripts/icetray/step1/pass3_update_gcd_icetopdomcal.py
@@ -0,0 +1,178 @@
+"""Update GCD for pass3 step 1 processing:
+
+* Replace bad IceTop DOMCals with good IceTop DOMCals from a nearby run.
+  This is something necessary for certain runs in IC86.2011
+
+Usage:  python pass3_update_gcd_chargecorr.py <ingcd> <stealfromgcd> <outgcd>
+"""
+
+#import math
+import sys
+from icecube import icetray, dataclasses, dataio #NOQA: F401
+from icecube.offline_filterscripts.gcd_generation import get_nan_doms, run_gcd_audit_pass3
+
+
+## --------------------------------------------
+## ------ FILE HANDLING: ----------------------
+## --------------------------------------------
+## Catalog of the first runs of every calendar year:
+fr_calyear_map = {
+    2011: 117294, 2012: 119220, 2013: 121480, 2014: 123614, 2015: 125791,
+    2016: 127345, 2017: 129003, 2018: 130476, 2019: 131986, 2020: 133575,    
+    2021: 134850, 2022: 136124, 2023: 137496, 2024: 138808, 2025: 140310
+}
+
+import glob
+def find_level2_gcd(r, forgivemissing=True):
+    # Find the appropriate GCD files
+    # First, narrow it down by year.
+    calyear = 2010 
+    for y in sorted(fr_calyear_map.keys()):
+        if fr_calyear_map[y] <= r:  # Maybe it's this one!
+            calyear = y
+        else:  # We passed it
+            break
+    print("Found it in calendar year", calyear)
+
+    # Now search for it.
+    if r < 129523: # before the start of IC86.2017
+        pattern = "/data/exp/IceCube/%d/filtered/level2pass2a/*/Run00%d/*GCD*"%(calyear, r)
+    else:
+        pattern = "/data/exp/IceCube/%d/filtered/level2/*/Run00%d/*GCD*"%(calyear, r)
+    gcdlist = glob.glob(pattern)
+    # Make sure there's only one of them.
+    if len(gcdlist)>1:
+        print("Searched but too many: ", pattern)
+        #if forgivemissing:  # This one is unforgivable?
+        #    return None
+        #else:
+        raise Exception("Too many found.")
+    if len(gcdlist)==0:
+        print("Searched but missing: ", pattern)
+        if forgivemissing:
+            return None
+        else:
+            raise Exception("None found.")
+    return gcdlist[0]
+
+
+## --------------------------------------------
+## ------ THE MODULE: -------------------------
+## --------------------------------------------
+
+class ReplaceIceTopDOMCals_fromOtherGCD(icetray.I3Module):
+    """
+    Replace the IceTop DOMCal's for a particular DOM with the values from a different GCD file.
+    """
+
+    def __init__(self, ctx):
+        super().__init__(ctx)
+        self.AddParameter('StealFromFile', 'GCD file containing good IceTop DOMCals to steal from', ""),
+        self.AddParameter('OMKeyList', 'List of OMKeys to steal', [])
+        self.AddOutBox('OutBox')
+
+    def Configure(self):
+        self.stealfile = self.GetParameter('StealFromFile')
+        self.omkeylist = self.GetParameter('OMKeyList')
+        if len(self.omkeylist) == 0:
+            ## If the list is empty, it means we'll do the entire array
+            for s in range(1,82):
+                for om in range(61,65):
+                    if (s != 39) or (om != 61):  # exclude the dead one
+                        self.omkeylist.append(icetray.OMKey(s, om))
+
+    # All the action takes place in the C-frame!
+    def Calibration(self, frame):
+        # The original object
+        calitem = frame['I3Calibration']
+
+        # Grab the geometry too, for error-checking
+        geo = frame["I3Geometry"]
+        geo_o = geo.omgeo
+
+        # Find the OTHER C-frame to steal from 
+        i3f = dataio.I3File(self.stealfile)
+        otherCframe = None
+        while (otherCframe == None):
+            f = i3f.pop_frame()
+            if f.Stop == icetray.I3Frame.Calibration:
+                otherCframe = f
+        othercal = otherCframe["I3Calibration"].dom_cal
+
+        # Go find each DOM to steal
+        for k in self.omkeylist:
+            # Make sure it's an IceTop DOM
+            if k not in geo.omgeo:
+                raise Exception("Hey, this DOM isn't in the Geometry!", k)
+            if geo.omgeo[k].omtype != dataclasses.I3OMGeo.IceTop:
+                raise Exception("Hey, this DOM is not an IceTop DOM!", k)
+
+            # Fetch it
+            cdom = othercal[k]
+            
+            # Copy it
+            icetray.i3logging.log_info(f"Copying DOMCal for DOM: {k}")
+            calitem.dom_cal[k] = cdom
+
+
+        # Replace the original with the new one in the
+        frame.Delete("I3Calibration")
+        frame["I3Calibration"] = calitem
+        self.PushFrame(frame)
+
+
+## --------------------------------------------
+## ------ SET INPUT AND OUTPUT: ---------------
+## --------------------------------------------
+## For testing
+infile = find_level2_gcd(119189, False)
+stealfile = find_level2_gcd(118175, False)
+outfile = "testme_119183_GCD.i3.bz2"
+
+'''
+if len(sys.argv) !=5:
+    icetray.logging.log_error(f"{sys.argv[0]} <ingcd> <outgcd> <ingcd_audit> <outgcd_audit>")
+    sys.exit(0)
+
+infile = sys.argv[1]
+stealfile = sys.argv[2]
+outfile = sys.argv[3]
+'''
+
+
+## --------------------------------------------
+## ------ EXECUTE IT: -------------------------
+## --------------------------------------------
+## Try actually running it!
+## ===========================
+
+icetray.logging.log_warn(f"Fixing GCD input file for Pass3: {infile}")
+icetray.logging.log_warn(f"Stealing from GCD: {stealfile} ")
+icetray.logging.log_warn(f"Writing GCD: {outfile} ")
+
+icetray.logging.set_level("INFO")
+
+
+# Set up the Tray.
+tray = icetray.I3Tray()
+
+# Read input
+icetray.i3logging.log_info("Reading from: %s"%infile)
+tray.AddModule("I3Reader", "readme", Filename=infile)
+
+#tray.AddModule("Dump", "dumpme")   # for testing!
+
+tray.Add(ReplaceIceTopDOMCals_fromOtherGCD, "replacetest",
+         StealFromFile = stealfile,
+         OMKeyList = [icetray.OMKey(1,61), icetray.OMKey(1,63)])  # try one or two
+#         OMKeyList = [])  # try all of them
+ 
+# Write output
+icetray.i3logging.log_info("Writing to: %s"%outfile)
+streams = [icetray.I3Frame.Geometry, icetray.I3Frame.Calibration,
+           icetray.I3Frame.TrayInfo,
+           icetray.I3Frame.DetectorStatus]
+tray.Add("I3Writer", filename=outfile, Streams=streams)
+
+# Execute!
+tray.Execute()
